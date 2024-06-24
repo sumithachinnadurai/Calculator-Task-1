f7278fc7fed877113efda42ff1ed81868d9f93d0
Sumitha <124781919+sumithachinnadurai@users.noreply.github.com>
2024-06-24 19:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    if (display.innerText.length > 1) {
+        display.innerText = display.innerText.slice(0, -1);
+    } else {
+        display.innerText = '0';
+    }
+}
+
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText.replace('x', '*'));
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
